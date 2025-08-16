194fccbd7cc0e3f9f64fecbe02d69e9e8eb20ec3
RafkaJulianee <rafkagenggkk@gmail.com>
2025-08-16 13:25:40 +0700

    update

--- crud/index.php
+++ crud/index.php
@@ -3,5 +3,6 @@ $nama="Rafka Julian Pratama";
 $hutang=30000;
 $tabungan=15000;
 
-echo "Haloo ".$nama."Hutang Anda Adalaah".  "<br>"   .$hutang;
+echo "Haloo, ".$nama."Hutang Anda Adalaah".  "<br>"   .$hutang;
+echo "Sisa Tabungan Anda". $tabungan;
 
