e4578c61be5f27872b192a0a453951d85744bcc6
RafkaJulianee <rafkagenggkk@gmail.com>
2025-08-19 09:47:59 +0700

    update

--- minggu2php/tabel.php
+++ minggu2php/tabel.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Membuat Tabel</title>
     <body>
+        <link rel="stylesheet" href="style.css">
         <style>
           body {
 
@@ -33,6 +34,7 @@
         echo $jumlah * $harga;
         echo "</td></tr>";
         $jumlah +=10;
+
 
 
     }
