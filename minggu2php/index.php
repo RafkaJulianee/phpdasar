2913f373ced6d5cffe64b8a11f5b9865da9ffac5
RafkaJulianee <rafkagenggkk@gmail.com>
2025-08-19 07:40:13 +0700

    update

--- minggu2php/index.php
+++ minggu2php/index.php
@@ -1,4 +1,31 @@
-<?php
-  echo "Test";
 
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+      body {
+
+       font-family: 'Inter', Arial, sans-serif;
+    }
+
+</style>
+<body>
+    <h1 id="judul" style="text-align:center">Belajar Php Menyenangkan😎</h1>
+    
+    <?php
+    //menampilkan angka 1-100
+    $angka=1;
+    while($angka <=100){
+        
+    }
+
+
+    ?>
+</body>
+</html>
+
+
