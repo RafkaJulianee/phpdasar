cb379d436fd3fe2948998db65e62c7a1ff42f064
RafkaJulianee <rafkagenggkk@gmail.com>
2025-08-20 10:51:41 +0700

    update

--- main.php
+++ main.php
@@ -1,8 +1,20 @@
 <?php
-function sapa($nama){
-    echo "Haloo,Selamat Datang".$nama;
+//fungsi dasar
+function sapa(){
+    echo "Haloo,Selamat Datang";
 }
-sapa('Rafka Julian Pratama');
+echo '<br>';
+//panggil fungsi
+sapa();
+//fungsi dengan parameter
+function salam($nama){
+    echo "Haloo, $nama! Apa Kabar?<br>";
+}
+salam("rafka");
+salam("rafka");
+salam("rafka");
+
+
 
 
 
