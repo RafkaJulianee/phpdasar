77bd11c00ae27d045ecbdf0a818322f482a17d81
RafkaJulianee <rafkagenggkk@gmail.com>
2025-09-02 09:42:08 +0700

    update

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+  // //menghitung luas perseg
+        // //rumus l= sisi*sisi
+        // var sisi = 10
+        // var luas = sisi * sisi;
+        // document.write("Luas Persegi Panjang " + "=" + luas)
+
+        
+        //menghitung luas lingkaran
+        // rumus l = phi*r*r dengan jari jari 2=2.3
+        // menghitung luas lingkaran
+        var r = 2.3;
+        var phi = 3.14;
+        var luas = phi * r * r;
+
+        document.writeln("Luas Lingkaran = " + luas);
