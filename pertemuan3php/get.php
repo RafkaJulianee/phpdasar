d237f009ce54f582a9c10fb1da68b69f141225d9
RafkaJulianee <rafkagenggkk@gmail.com>
2025-09-14 13:52:40 +0700

    update

--- pertemuan3php/get.php
+++ pertemuan3php/get.php
@@ -1,14 +1,27 @@
 <!DOCTYPE html>
 <?php
 
+
 ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cossette+Titre:wght@400;700&family=Dancing+Script:wght@400..700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
 </head>
 <body>
+    <div class="navbar">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+    </div>
+    <h1>Hallo World</h1>
+
     
 </body>
 </html>
