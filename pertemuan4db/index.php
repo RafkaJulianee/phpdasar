940518858a539bbf328a21171ac9a289264a2c33
RafkaJulianee <rafkagenggkk@gmail.com>
2025-09-15 09:39:22 +0700

    update

--- pertemuan4db/index.php
+++ pertemuan4db/index.php
@@ -7,6 +7,11 @@ include "koneksi.php";
   <meta charset="UTF-8">
   <title>Data Siswa XIRPL1</title>
   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Cossette+Titre:wght@400;700&family=Dancing+Script:wght@400..700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
 </head>
 <body>
   <h1>Data Siswa XIRPL1</h1>
@@ -33,7 +38,6 @@ include "koneksi.php";
           }
           echo "</tr>";
 
-          // isi data
           while ($row = mysqli_fetch_assoc($data)) {
               echo "<tr>";
               foreach ($fields as $field) {
