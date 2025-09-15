f064788af1f17a4f290b6a5c4be3c283bd93efde
RafkaJulianee <rafkagenggkk@gmail.com>
2025-09-15 11:46:59 +0700

    update

--- form/index.php
+++ form/index.php
@@ -4,13 +4,17 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Data Siswa</title>
+    <link rel="stylesheet" href="CSS/style.css">
     <link rel="shortcut icon" href="img/profil1.png" type="image/x-icon">
-    <link rel="stylesheet" href="css/style.css">
-     <link rel="preconnect" href="https://fonts.googleapis.com">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Cossette+Titre:wght@400;700&family=Dancing+Script:wght@400..700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-</head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+   <style>
+    body{
+      font-family: "Montserrat";
+    }
+   </style>
   </head>
   <body>
     <div class="container">
@@ -28,6 +32,29 @@
 
             </tr>
         </thead>
+        <tbody>
+            <?php
+            include "koneksi.php";
+            $no=1;
+            $query = mysqli_query($conn, "SELECT * FROM siswa");
+            while($row=mysqli_fetch_assoc($query)){
+                 ?>
+                 <tr>
+                    <td><?=$no++;?></td>
+                    <td><?=$row['nis'];?></td>
+                    <td><?=$row['nama'];?></td>
+                    <td><?=$row['kelas'];?></td>
+                    <td><?=$row['jenis_kelamin'];?></td>
+                    <td><?=$row['alamat'];?></td>
+                    <td><?=$row['no_hp'];?></td>
+                 </tr>
+
+            <?php
+            
+        }
+              ?>
+       
+        </tbody>
 
      </table>
     </div>
--- form/koneksi.php
+++ form/koneksi.php
@@ -8,10 +8,7 @@ $conn = mysqli_connect($host, $user, $password, $db);
 
 if (!$conn) {
     die("Koneksi GAGAL: " . mysqli_connect_error());
-} else {
-    echo "<h1>Koneksi Berhasil🚀</h1>";
-}
-
+} 
 
 ?>
 
