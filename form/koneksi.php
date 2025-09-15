15850c1b079666701f6e601bd4742dbd100eccdc
RafkaJulianee <rafkagenggkk@gmail.com>
2025-09-15 10:19:40 +0700

    update

--- form/koneksi.php
+++ form/koneksi.php
@@ -1,10 +1,10 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = "";
+$password = "";
 $db   = "db_siswarpl1";
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $password, $db);
 
 if (!$conn) {
     die("Koneksi GAGAL: " . mysqli_connect_error());
@@ -13,23 +13,3 @@ if (!$conn) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Cossette+Titre:wght@400;700&family=Dancing+Script:wght@400..700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-<style>
-    body{
-           font-family: "Montserrat";
-    }
-</style>
-</head>
-</head>
-<body>
-    
-</body>
-</html>
