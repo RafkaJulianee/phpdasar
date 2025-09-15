c0b10af01dc96a86b543cac7ad5ac08669a764f9
RafkaJulianee <rafkagenggkk@gmail.com>
2025-09-15 10:05:18 +0700

    update

--- pertemuan4db/index.php
+++ pertemuan4db/index.php
@@ -13,7 +13,6 @@ include "koneksi.php";
      <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Cossette+Titre:wght@400;700&family=Dancing+Script:wght@400..700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-
 </head>
 </head>
 <body>
