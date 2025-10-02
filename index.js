819291826de9f2a12e1659c863fb9cdf25864ed0
RafkaJulianee <rafkagenggkk@gmail.com>
2025-10-02 19:28:50 +0700

    update

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ player ={
     position : "MIDFIELDER",
     nationality : "HUNGARY",
     img: "https://upload.wikimedia.org/wikipedia/commons/1/11/Dominik_Szoboszlai_2020.jpg"
-
-
 }
+document.writeln(player.name);
+document.writeln("<br>");
+document.writeln(player.age);
+document.writeln("<br>");
+document.writeln(player.club);
+document.writeln("<br>");
+document.writeln(player.position);
+document.writeln("<br>");
+document.writeln(player.nationality);
+document.writeln("<br>");
