d07b63249624d6a65538f6f4757413370b7f6dae
RafkaJulianee <rafkagenggkk@gmail.com>
2025-10-02 19:28:12 +0700

    update

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ player ={
     age : 22,
     club : "RB LEIPZIG",
     position : "MIDFIELDER",
-    nationality : "HUNGARY"
+    nationality : "HUNGARY",
+    img: "https://upload.wikimedia.org/wikipedia/commons/1/11/Dominik_Szoboszlai_2020.jpg"
 
 
 }
