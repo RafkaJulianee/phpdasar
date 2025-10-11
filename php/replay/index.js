2a92807cd857a8ef611bdd077da84318050e6e48
RafkaJulianee <rafkagenggkk@gmail.com>
2025-10-11 21:17:41 +0700

    up

--- php/replay/index.js
+++ php/replay/index.js
@@ -1,12 +1,8 @@
-let speed = 95;
-let mbappe = "Kylian Mbappe";
 
-function getPlayerRank(speed) {
-    if (speed === 95) {
-        return "S Rank";
-    }
-    // Add more conditions for other ranks if needed
-    return "Unknown Rank";
+
+player ={
+    name : "DOMINIK SZOBOZLAI",
+    age : 22,
+    club : "RB LEIPZIG",
 }
 
-document.writeln(getPlayerRank(speed));
